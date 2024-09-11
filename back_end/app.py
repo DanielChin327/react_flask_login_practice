5d4f20b71e45a50b9acf8f57bf2eb7708e20b9ac
Daniel Chin <DanChin327@gmail.com>
2024-09-11 14:22:59 +0900

    cont pract

---
+++ back_end/app.py
@@ -0,0 +1,18 @@
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+
+db = sqlalchemy.create_engine("mariadb+pymysql://root:@172.23.96.1:3306/northwinddb", echo = True)
+
+def get_customers():
+    with db.connect() as conn:
+        result = conn.execute(sqlalchemy.text("SELECT * FROM moviedb"))
+        print(result.all())
+
+def main():
+    get_customers()
+
+if __name__ == '__main__':
+    main()
